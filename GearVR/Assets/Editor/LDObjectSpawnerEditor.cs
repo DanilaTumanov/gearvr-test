6b0471987610d6223b345ffcbe5d9d218ebe4a92
DanT <danilatumanov@gmail.com>
2018-06-13 23:24:43 +0300

    Добавил спаунер объектов и тестовую сцену

---
+++ GearVR/Assets/Editor/LDObjectSpawnerEditor.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(LDObjectSpawner))]
+public class LDObjectSpawnerEditor : Editor {
+
+
+    private LDObjectSpawner _target;
+
+
+        
+
+    public override void OnInspectorGUI()
+    {
+        _target = (LDObjectSpawner) target;
+
+        DrawDefaultInspector();
+            
+
+    }
+
+
+    private void OnSceneGUI()
+    {
+            
+        ProcessPlaceObject();
+
+    }
+
+
+
+    private void ProcessPlaceObject()
+    {
+        int controlID = GUIUtility.GetControlID(FocusType.Passive);
+        Event currentEvent = Event.current;
+
+        // Если нажата ЛКМ
+        if (currentEvent.type == EventType.MouseDown && currentEvent.button == 0)
+        {
+            // Перехватываем событие своим контролом
+            GUIUtility.hotControl = controlID;
+
+
+            RaycastHit hit;
+            // TODO: Сделать возможность выбирать слои на которых размещать и вынести в отдельный метод. Не надо каждый раз считать маску
+            LayerMask layerMask = 1 << LayerMask.NameToLayer("Ground");
+
+                
+            // TODO: 1000 - волшебная константа - плохо.
+            if (Physics.Raycast(HandleUtility.GUIPointToWorldRay(currentEvent.mousePosition), out hit, 10000))
+            {
+                int hitLayerMask = 1 << hit.transform.gameObject.layer;
+                    
+                // Проверяем кликнули ли мы на слой, на котором можно размещать наш GO.
+                if ((hitLayerMask & layerMask) == hitLayerMask)
+                {
+                    SpawnAtPoint(hit.point);
+                }                        
+            }
+
+            currentEvent.Use();
+        }
+        else if (currentEvent.type == EventType.MouseUp)
+        {
+            // По отжатию кнопки мыши отпускаем перехват событий
+            GUIUtility.hotControl = 0;
+        }
+    }
+
+
+    // Расположить объект(ы) относительно точки по заданным настройкам
+    private void SpawnAtPoint(Vector3 point)
+    {
+        // Создаем вектор вращения на основе настроек
+        var rot = Quaternion.Euler(
+                _target.randomXRotation ? Random.Range(0, 360) : _target.rotation.x,
+                _target.randomYRotation ? Random.Range(0, 360) : _target.rotation.y,
+                _target.randomZRotation ? Random.Range(0, 360) : _target.rotation.z
+            );
+
+        GameObject instance = Instantiate(_target.prefab, point, rot, _target.parent);
+    }
+
+        
+}
+	
---
+++ GearVR/Assets/Scripts/Tools/LDObjectSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/// <summary>
+/// Компонент для удобной расстановки объектов
+/// </summary>
+[ExecuteInEditMode]
+[DisallowMultipleComponent]
+public class LDObjectSpawner : BaseObjectScene {
+    
+    [Header("Основные настройки")]
+	public GameObject prefab;
+    public Transform parent;
+
+    [Header("Вращение")]
+    public bool randomXRotation = false;
+    public bool randomYRotation = false;
+    public bool randomZRotation = false;
+    public Vector3 rotation = Vector3.zero;
+
+}
+	
